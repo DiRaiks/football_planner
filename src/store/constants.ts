a3e96637f6d46d964614628b0b0fac3a7073a731
DiRaiks <t.fane@mail.ru>
2019-10-01 20:58:37 +0300

    update requests path

--- src/store/constants.ts
+++ src/store/constants.ts
@@ -1,1 +1,1 @@
-export const FETCH_URL: string = 'http://87.236.22.136:3000';
+export const FETCH_URL: string = 'http://192.168.0.102:3000';
--- src/store/table/tableApi.ts
+++ src/store/table/tableApi.ts
@@ -3,21 +3,21 @@ import axios from 'axios';
 import { PeopleItem } from '@/store/types';
 import { FETCH_URL } from '@/store/constants';
 
-export const getPeopleData = () => axios.get(`${FETCH_URL}/getPeopleData`)
+export const getPeopleData = () => axios.get(`${FETCH_URL}/api/people/getPeopleData`)
     .then((response) => response.data);
-export const getPeopleByDate = (date: string) => axios.post(`${FETCH_URL}/getPeoplesByDate`, { date })
+export const getPeopleByDate = (date: string) => axios.post(`${FETCH_URL}/api/people/getPeoplesByDate`, { date })
     .then((response) => response.data);
-export const savePeople = (people: PeopleItem) => axios.post(`${FETCH_URL}/savePeople`, { people })
+export const savePeople = (people: PeopleItem) => axios.post(`${FETCH_URL}/api/people/savePeople`, { people })
     .then((response) => response.data);
-export const deletePeople = (id: string) => axios.post(`${FETCH_URL}/deletePeople`, { id })
+export const deletePeople = (id: string) => axios.post(`${FETCH_URL}/api/people/deletePeople`, { id })
     .then((response) => response.data);
 export const saveEvent = (time: string, place: string, date: string, minimum: number) => axios
-    .post(`${FETCH_URL}/saveEvent`, { time, place, date, minimum })
+    .post(`${FETCH_URL}/api/events/saveEvent`, { time, place, date, minimum })
     .then((response) => response.data);
-export const getEvents = () => axios.get(`${FETCH_URL}/getEvent`)
+export const getEvents = () => axios.get(`${FETCH_URL}/api/events/getEvent`)
     .then((response) => response.data);
-export const deleteEvent = (id: string) => axios.post(`${FETCH_URL}/deleteEvent`, { id })
+export const deleteEvent = (id: string) => axios.post(`${FETCH_URL}/api/events/deleteEvent`, { id })
     .then((response) => response.data);
 export const saveEventMinimum = (eventId: string, minimum: number) => axios
-    .post(`${FETCH_URL}/saveEventMinimum`, { eventId, minimum })
+    .post(`${FETCH_URL}/api/events/saveEventMinimum`, { eventId, minimum })
     .then((response) => response.data);
